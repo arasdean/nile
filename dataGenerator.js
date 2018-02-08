81d7895b0ae27bb18282c36490a24951877848bb
arasdean <arasdean@gmail.com>
2018-02-07 20:06:35 -0800

    Comments on datagen file for clarity

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,5 +1,16 @@
 const cassandra = require('cassandra-driver');
 const faker = require('faker');
+/*
+NOTE: 
+
+This generation script requires the manual changing of the following ranges: 
+	-user_id
+	-product_id
+	-quant
+	-id
+	-ordered_at 
+ */ 
+
 var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'testing2'});
 var createCsvWriter = require('csv-writer').createObjectCsvWriter;
 var csvWriter = createCsvWriter({
@@ -13,12 +24,13 @@ var csvWriter = createCsvWriter({
   ]
 })
 
-let records = [];
-for (var i = 0; i < 10000; i++) {
+let records = []; 
+for (var i = 0; i < 1000000; i++) {
     var randomUserId = Math.round(Math.random() * (10000000 - 9000001) + 9000001)
     var randomQuant = Math.round(Math.random() * (200 -1) + 1);
     var randomProduct = Math.round(Math.random() * (10000000 - 1) + 1)
-    // client.execute("insert into orders (id, user_id, user_email, product_id, quant, viewed_at) values (uuid(), " + randomUserId + ", '" + randomEmail + "'," + randomProduct + "," + randomQuant + ", dateof(now()));")
+// Below is the former query that I used to brute force insert into the DB before copying CSV file     
+// client.execute("insert into orders (id, user_id, user_email, product_id, quant, viewed_at) values (uuid(), " + randomUserId + ", '" + randomEmail + "'," + randomProduct + "," + randomQuant + ", dateof(now()));")
 
     let record = {
       user_id: randomUserId,
