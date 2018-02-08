ae5e7bd69d76cb390747fc4d6acff84a70ef03aa
arasdean <arasdean@gmail.com>
2018-02-08 12:01:44 -0800

    Inserted 10 mill datapts and fixed orders schema

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,17 +1,15 @@
 const cassandra = require('cassandra-driver');
 const faker = require('faker');
 /*
-NOTE: 
+NOTE:
 
-This generation script requires the manual changing of the following ranges: 
+This generation script requires the manual changing of the following ranges:
 	-user_id
 	-product_id
 	-quant
 	-id
-	-ordered_at 
- */ 
-
-var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'testing2'});
+	-ordered_at
+ */
 var createCsvWriter = require('csv-writer').createObjectCsvWriter;
 var csvWriter = createCsvWriter({
   path: __dirname + '/ordersTotal.csv',
@@ -23,25 +21,24 @@ var csvWriter = createCsvWriter({
     {id: 'ordered_at', title: 'ordered_at'},
   ]
 })
-
-let records = []; 
+let records = [];
 for (var i = 0; i < 1000000; i++) {
-    var randomUserId = Math.round(Math.random() * (10000000 - 9000001) + 9000001)
-    var randomQuant = Math.round(Math.random() * (200 -1) + 1);
-    var randomProduct = Math.round(Math.random() * (10000000 - 1) + 1)
-// Below is the former query that I used to brute force insert into the DB before copying CSV file     
-// client.execute("insert into orders (id, user_id, user_email, product_id, quant, viewed_at) values (uuid(), " + randomUserId + ", '" + randomEmail + "'," + randomProduct + "," + randomQuant + ", dateof(now()));")
 
+    var randomUserId = Math.round(Math.random() * (10000000 - 9000001) + 9000001)
+    var randomQuant = Math.round(Math.random() * (250 -1) + 1);
+    var randomProduct = Math.round(Math.random() * (10000000 - 1) + 1)
+    var date = faker.date.between('2017-12-01', '2018-02-27').toISOString();
     let record = {
       user_id: randomUserId,
       product_id: randomProduct,
       quant: randomQuant,
       id: faker.random.uuid(),
-      ordered_at: faker.date.recent().getTime()
+      ordered_at: date,
     }
 
     records[i] = record;
 }
+
 
 csvWriter.writeRecords(records)
 .then(()=>{
@@ -49,5 +46,13 @@ csvWriter.writeRecords(records)
 })
 .catch((err) => {console.error(err)})
 
-// Written in Shell:
-// COPY orders (user_id, product_id, quant, id, ordered_at) FROM '/Users/arasdean/Desktop/Hack_Reactor/thesis/orders.csv';
+
+/*
+ ---Written in Shell:---
+ COPY orders (user_id, product_id, quant, id, ordered_at) FROM '/Users/arasdean/Desktop/Hack_Reactor/thesis/orders.csv';
+
+NOTE
+Below is the former query that I used to brute force insert into the DB before copying CSV file:
+
+ client.execute("insert into orders (id, user_id, user_email, product_id, quant, viewed_at) values (uuid(), " + randomUserId + ", '" + randomEmail + "'," + randomProduct + "," + randomQuant + ", dateof(now()));")
+ */
